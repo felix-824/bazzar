d0cee00c8ac074cba736752b636c819cf8cd3cf9
felix-824 <nf94new@gmail.com>
2026-08-28 23:45:35 +0900

    feat: add order models and types

---
+++ src/libs/types/order.ts
@@ -0,0 +1,42 @@
+import { ObjectId } from 'mongoose';
+import { OrderStatus } from '../enums/order.enum';
+
+//Order DB'dagi umumiy order
+export interface Order {
+  _id: ObjectId;
+  orderTotal: number;
+  orderDelivery: number;
+  orderStatus: OrderStatus;
+  memberId: ObjectId;
+  createdAt: Date;
+  updatedAt: Date;
+}
+//OrderItem order ichidagi bitta mahsulot
+export interface OrderItem {
+  _id: ObjectId;
+  itemQuantity: number; // nechta mahsulot
+  itemPrice: number; // bitta mahsulotning narxi
+  orderId: ObjectId;
+  productId: ObjectId;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+//OrderItemInput yangi item yaratish uchun input
+export interface OrderItemInput {
+  itemQuantity: number;      // nechta mahsulot?
+  itemPrice: number;        // mahsulot narxi
+  productId: ObjectId;       // qaysi product?
+  orderId?: ObjectId;        // qaysi order?
+}
+
+//OrderInquiry orderlarni qidirish/filterlash uchun
+export interface OrderInquiry {
+    page: number;
+    limit: number;
+    orderStatus?: OrderStatus;
+}
+
+export interface OrderUpdateInput{
+    orderStatus: OrderStatus;
+}
